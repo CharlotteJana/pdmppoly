3abdcc3e24d562e9c46f29d758f1ba4ecc304894
Charlotte Jana <charlotte.jana@t-online.de>
2019-04-16 19:28:52 +0200

    test for lognormal distribution with n = 1

--- R/momApp_momentClosure.R
+++ R/momApp_momentClosure.R
@@ -5,37 +5,49 @@
 
 ########### moment closure ##############
 
-#' Calculate moments
+#' Symbolic calculation of moments
 #'
-#' Compute central moments of a multivariate variable \eqn{X = (X_1, ..., X_n)}.
+#' Compute the moments of a multivariate variable \eqn{X = (X_1, ..., X_n)}
+#' and return the formula as quoted expression.
 #'
 #' @param mean vector or list with expected values. Entry \code{mean[[i]]} should
 #' contain a value for \eqn{E(X_i)}.
 #' @param cov matrix or nested list. Entry \code{cov[i][j]} (or \code{cov[[i]][[j]]} 
-#' respectivly) should contain a #' value for the covariance \eqn{Cov(X_i, X_j)}.
+#' respectivly) should contain a value for the covariance \eqn{Cov(X_i, X_j)}.
 #' @param var optional. If \code{n = 1}, \code{cov} would only
 #' contain one entry - the variance \eqn{Var(X)}. This value can be passed
 #' to parameter \code{var} instead of \code{cov}.
-#' @param missingOrders numeric matrix. Each row gives the order
+#' @param missingOrders numeric vector or matrix. Each row gives the order
 #' of a moment that shall be calculated.
 #' @param distribution string specifying the (multivariate) distribution
 #' of X. The following values are possible: 
 #' \itemize{
-#' \item \code{zero} sets all moments to 0,
-#' \item \code{normal} sets all moments as moments of a centralized normal distribution,
-#' \item \code{lognormal} sets all moments as moments of a lognormal distribution.
+#' \item \code{"zero"} sets all moments to 0,
+#' \item \code{"normal"} calculates the moments of a centralized normal distribution,
+#' \item \code{"lognormal"} calculates the raw moments of a lognormal distribution,
+#' \item \code{"gamma"} calculates the raw moments of a gamma distribution.
 #' } 
 #' @return A list where each element is a quoted expression.
 #' The i-th element of this list gives a formula for the
 #' moment whose order is given in the i-th row of \code{missingOrders}.
+#' @note The calculation of the central moments of a multivariate normal distribution
+#' is based on function \code{\link[symmoments]{callmultmoments}} of package \pkg{symmoments}.
 #' @importFrom symmoments callmultmoments
 #' @importFrom stringr str_extract_all
 #' @export
 momentClosure <- function(distribution, missingOrders, mean = NA, cov = NA, var = NA){
   
+  # definitions
   if(is.vector(missingOrders))
     missingOrders <- matrix(missingOrders, nrow = 1)
   n <- ncol(missingOrders)
+  missingMoments <- rep(list(NA), nrow(missingOrders))
+  
+  # zero: all moments = 0
+  if(distribution == "zero"){
+    missingMoments <- rep(list(0), nrow(missingOrders))
+    return(missingMoments)
+  }
   
   # validate cov
   if(!is.na(var))
@@ -49,14 +61,6 @@ momentClosure <- function(distribution, missingOrders, mean = NA, cov = NA, var 
         stop("The covariance matrix 'cov' should be symmetric.")
   }
   
-  # definitions
-  missingMoments <- rep(list(NA), nrow(missingOrders))
-  n <- ncol(missingOrders)
-  
-  # zero: all moments = 0
-  if(distribution == "zero"){
-    missingMoments <- rep(list(0), nrow(missingOrders))
-  }
   
   # normal: moments are 0 (if sum(order) is odd) or calculated with package symmoments (if sum(order) is even)
   if(distribution == "normal"){
@@ -85,16 +89,21 @@ momentClosure <- function(distribution, missingOrders, mean = NA, cov = NA, var 
         missingMoments[[i]] <- momFormula
       }
     }
+    return(missingMoments)
   }
   
   if(distribution == "lognormal"){ 
     sigma <- cov
     mu <- list()
     for(i in 1:n){
-      sigma[[i]][[i]] <- bquote(log(1+.(cov[[i]][[i]])/.(mean[i])^2))
-      mu[[i]] <- bquote(log(.(mean[i])-0.5*.(sigma[[i]][[i]])))
+      sigma[[i]][[i]] <- list(bquote(log(1+.(cov[[i]][[i]])/.(mean[i])^2)))
+      sigma[[i]][[i]] <- sigma[[i]][[i]][[1]]
+      mu[[i]] <- bquote(log(.(mean[i]))-0.5*.(sigma[[i]][[i]]))
+      cat("mu = ", eval(mu[[i]]), "var = ", eval(sigma[[i]][[i]]), "\n")
       for(j in seq_len(i-1)){
-        sigma[[i]][[j]] <- bquote(log(1 + .(cov[[i]][[j]])/exp(.(mu[[i]])+.(mu[[j]])+0.5*(.(sigma[[i]][[i]])*.(sigma[[j]][[j]])))))
+        sigma[[i]][[j]] <- bquote(
+          log(1 + .(cov[[i]][[j]])/exp(.(mu[[i]])+.(mu[[j]])+0.5*(.(sigma[[i]][[i]])*.(sigma[[j]][[j]]))))
+          )
         sigma[[j]][[i]] <- sigma[[i]][[j]]
       }
     }
@@ -104,15 +113,19 @@ momentClosure <- function(distribution, missingOrders, mean = NA, cov = NA, var 
       summand1 <- Reduce(function(a,b) bquote(.(a)+.(b)), summand1)
       summand2 <- lapply(1:n, function(i) lapply(i:n, function(j){ 
                                                         if(i == j) 
-                                                          bquote(.(order[i])^2*.(cov[[i]][[i]]))
+                                                          bquote(.(order[i])^2*.(sigma[[i]][[i]]))
                                                         else 
-                                                          bquote(2*.(order[i])*.(order[j])*.(cov[[i]][[j]]))
+                                                          bquote(2*.(order[i])*.(order[j])*.(sigma[[i]][[j]]))
                                                  })
                          )
       summand2 <- Reduce(c, summand2) # flatten the nested list
       summand2 <- Reduce(function(a,b) bquote(.(a)+.(b)), summand2)
       missingMoments[[i]] <- bquote(exp(.(summand1)+0.5*.(summand2)))
     }
+  }
+  
+  if(distribution == "gamma"){
+    stop("distribution = 'gamma' is not implemented yet")
   }
   
   # normal for one dimensional variable
