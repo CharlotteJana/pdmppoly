fe223c7617576b673be9eeffdd0161b1fad41a0d
Charlotte Jana <charlotte.jana@t-online.de>
2019-04-16 14:13:44 +0200

    momentClosure of normal distribution with symmoments

--- R/momApp_momentClosure.R
+++ R/momApp_momentClosure.R
@@ -1,3 +1,5 @@
+#t1 wird die variable mean überhaupt benötigt?
+
 ########### moment closure ##############
 
 #' Calculate moments
@@ -17,17 +19,29 @@
 #' of X. The following values are possible: \code{zero} sets
 #' all moments to 0, \code{normal} sets all moments as moments of
 #' a centralized normal distribution.
-#' @value A list where each element is a quoted expression.
+#' @return A list where each element is a quoted expression.
 #' The i-th element of this list gives a formula for the
 #' moment whose order is given in the i-th row of \code{missingOrders}.
+#' @importFrom symmoments callmultmoments
+#' @importFrom stringr str_extract_all
+#' @export
 momentClosure <- function(distribution, missingOrders, mean = NA, cov = NA, var = NA){
   
   if(is.vector(missingOrders))
     missingOrders <- matrix(missingOrders, nrow = 1)
+  n <- ncol(missingOrders)
+  
+  # validate cov
   if(!is.na(var))
     cov <- var
   if(is.matrix(cov))
     cov <- apply(cov, 1, as.list)
+  stopifnot(n == length(cov))
+  for(i in 1:n){
+    for(j in 1:n)
+      if(cov[[i]][[j]] != cov[[j]][[i]])
+        stop("The covariance matrix 'cov' should be symmetric.")
+  }
   
   # definitions
   missingMoments <- rep(list(NA), nrow(missingOrders))
@@ -38,8 +52,36 @@ momentClosure <- function(distribution, missingOrders, mean = NA, cov = NA, var 
     missingMoments <- rep(list(0), nrow(missingOrders))
   }
   
+  if(distribution == "normal"){
+    for(i in seq_len(nrow(missingOrders))){
+      order <- as.numeric(missingOrders[i, ])
+      if(sum(order) %% 2 == 1){
+        missingMoments[[i]] <- 0
+      }
+      else{
+        moment <- symmoments::callmultmoments(order) # calculation of the moment
+        names(moment$coefficients) <- NULL
+        momFormula <- list()
+        for(j in length(moment$coefficients)){
+          factors <- lapply(1:(n*(n+1)/2), function(r){
+            power <- moment$representation[j, r]
+            index <- grep("[0-9]", names(moment$representation[j, ])[r], value = TRUE)
+            index <- as.numeric(stringr::str_extract_all(index, "[[:digit:]]")[[1]])
+            if(power != 0) bquote(.(cov[[index[1]]][[index[2]]])^.(power))
+          })
+          factors[sapply(factors, is.null)] <- NULL
+          factors <- Reduce(function(a,b) bquote(.(a)*.(b)), factors)
+          momFormula <- append(momFormula,
+                               bquote(.(moment$coefficients[j])*.(factors)))
+        }
+        momFormula <- Reduce(function(a,b) bquote(.(a)+.(b)), momFormula)
+        missingMoments[[i]] <- momFormula
+      }
+    }
+  }
+  
   # normal for one dimensional variable
-  if(distribution == "normal" & n == 1){
+  if(distribution == "normal1d" & n == 1){
     for(i in seq_len(nrow(missingOrders))){
       order <- as.numeric(missingOrders[i, 1])
       # sigmaRowIndex <- prodlim::row.match(2, knownOrders) #t2 check if 2nd moment is  contained in lhs
@@ -48,37 +90,6 @@ momentClosure <- function(distribution, missingOrders, mean = NA, cov = NA, var 
       missingMoments[[i]] <- switch(order %% 2,
                                     bquote(.(variance)^.(order)*.(dfactorial(order-1))),
                                     0)
-    }
-  }
-  
-  # normal: moments of a centralized normal distribution
-  if(distribution == "normal"){
-    for(i in seq_len(nrow(missingOrders))){
-      order <- missingOrders[i, ]
-      if(sum(order) %% 2 == 1){
-        missingMoments[[i]] <- 0
-      }
-      else{
-       k <- sum(order)
-       pairs <- combn(1:k, 2)
-       pairCombinations <- combn(1:ncol(pairs), k/2)
-       combinations <- NULL
-       for(i in seq_len(ncol(pairCombinations))){
-         combi <- as.vector(sapply(pairCombinations[, i], function(j) pairs[, j]))
-         if(length(unique(combi)) == length(combi))
-           combinations <- rbind(combinations, combi)
-       }
-       I <- rep(1:n, order)
-       combinations <- apply(combinations, 2, function(i) I[i])
-       sum <- list()
-       for(i in seq_len(nrow(combinations))){
-         factor <- lapply(1:(k/2), function(j) cov[[combinations[i, 2*j-1]]][[combinations[i,2*j]]])
-         factor <- Reduce(function(a,b) bquote(.(a)*.(b)), factor)
-         sum <- append(sum, factor)
-       }
-       sum <- Reduce(function(a,b) bquote(.(a)+.(b)), sum)
-       return(sum)
-      }
     }
   }
   
